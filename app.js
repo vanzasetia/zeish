3cb5ca5432434d8ea701b535bcf9a9887719bde0
Vanza Setia <venusbumi2@gmail.com>
2023-02-28 10:33:58 +0700

    Create a functionality to show top phrases
    This commit adds two new features to Zeish:
    
    - Users can see the top two-words
    - Users can see the top three-words
    
    This commit also makes the top-words leaderboard to be generated when
    the users want to generate them. Using `input` event creates lagging.
    So, doing this can prevent that from happening.
    
    Improve the styling and also the JavaScript code readability and
    maintainability by using class selectors instead of type selectors.

--- app.js
+++ app.js
@@ -1,25 +1,58 @@
 (function () {
   "use strict";
-  // set up the application
   const textField = document.querySelector("textarea");
-  const counter = `
+
+  const createButton = (text) => {
+    const button = document.createElement("button");
+    button.setAttribute("type", "button");
+    button.textContent = text;
+    return button;
+  };
+
+  const setUpApplication = () => {
+    const generateTopWordsButton = createButton("Generate top words");
+    generateTopWordsButton.classList.add("js-generate-top-keywords");
+
+    const counter = `
   <div class="counter">
-    <h2>Statistics</h2>
+    <h2 class="counter__title">Statistics</h2>
     <p>Characters (with spaces): <span class="js-total-characters">0</span></p>
     <p>Characters (no spaces): <span class="js-total-characters-without-spaces">0</span></p>
     <p>Words: <span class="js-total-words">0</span></p>
     <p>Lines: <span class="js-total-lines">0</span></p>
   </div>
   `;
-  const topKeywords = `
-  <div class="top-keywords">
-    <h2>Top keywords</h2>
-    <ol type="1"></ol>
+    const topKeywords = `
+  <div class="leaderboard">
+    <h2 class="leaderboard__title">Top keywords</h2>
+    <ol type="1" class="leaderboard__list js-leaderboard-keywords"></ol>
   </div>
   `;
-  textField.insertAdjacentHTML("afterend", topKeywords);
-  textField.insertAdjacentHTML("afterend", counter);
+    const topPhrasesTwoWords = `
+  <div class="leaderboard">
+    <h2 class="leaderboard__title">The top two-word phrases</h2>
+    <ol type="1" class="leaderboard__list js-leaderboard-two-words"></ol>
+  </div>
+  `;
 
+    const topPhrasesThreeWords = `
+  <div class="leaderboard">
+    <h2 class="leaderboard__title">The top three-word phrases</h2>
+    <ol type="1" class="leaderboard__list js-leaderboard-three-words"></ol>
+  </div>
+  `;
+
+    textField.insertAdjacentHTML("afterend", topPhrasesThreeWords);
+    textField.insertAdjacentHTML("afterend", topPhrasesTwoWords);
+    textField.insertAdjacentHTML("afterend", topKeywords);
+    textField.insertAdjacentHTML("afterend", counter);
+    textField.insertAdjacentElement("afterend", generateTopWordsButton);
+  };
+  setUpApplication();
+
+  const showTopWordsButton = document.querySelector(
+    ".js-generate-top-keywords"
+  );
   const totalCharacters = document.querySelector(".js-total-characters");
   const totalCharactersWithoutSpaces = document.querySelector(
     ".js-total-characters-without-spaces"
@@ -27,6 +60,7 @@
   const totalWords = document.querySelector(".js-total-words");
   const totalLines = document.querySelector(".js-total-lines");
   const wordRegEx = /[a-zA-Z\d]{1,}/g;
+  const EMPTY = 0;
 
   const countCharacters = () => {
     const text = textField.value;
@@ -35,9 +69,10 @@
 
   const countCharactersWithoutSpaces = () => {
     const text = textField.value;
-    const textWithoutSpaces = text.match(/\S/g);
+    const notSpaceRegEx = /\S/g;
+    const textWithoutSpaces = text.match(notSpaceRegEx);
     if (textWithoutSpaces === null) {
-      totalCharactersWithoutSpaces.textContent = 0;
+      totalCharactersWithoutSpaces.textContent = EMPTY;
     } else {
       totalCharactersWithoutSpaces.textContent = textWithoutSpaces.length;
     }
@@ -47,7 +82,7 @@
     const text = textField.value;
     const words = text.match(wordRegEx);
     if (words === null) {
-      totalWords.textContent = 0;
+      totalWords.textContent = EMPTY;
     } else {
       totalWords.textContent = words.length;
     }
@@ -55,7 +90,8 @@
 
   const countLines = () => {
     const text = textField.value;
-    const lines = text.match(/\n/g);
+    const lineFeedRegEx = /\n/g;
+    const lines = text.match(lineFeedRegEx);
     const FIRST_LINE = 1;
     if (lines === null) {
       totalLines.textContent = FIRST_LINE;
@@ -92,28 +128,50 @@
     return sortable;
   };
 
-  const showTopKeywords = () => {
-    const text = textField.value;
-    const lowerCaseText = text.toLowerCase();
-    const words = lowerCaseText.match(wordRegEx);
-    const list = document.querySelector('ol[type="1"');
+  const showStatistic = (words, list, styling = "") => {
     if (words === null) {
       list.innerHTML = "";
     } else {
       const wordsStatistic = getWordsStatistic(words);
       const sortedWordsStatistic = sortWordsStatistic(wordsStatistic);
-      let descriptionListHTML = sortedWordsStatistic
+      const listItems = sortedWordsStatistic
         .map((singleWordStatistic) => {
           const word = singleWordStatistic[0];
           const total = singleWordStatistic[1];
-          return `<li>
-                    <span>${word}</span>
-                    <span>Total: ${total}</span>
+          return `<li class="leaderboard__item ${styling}">
+                    <span class="leaderboard__word">${word}</span>
+                    <span class="leaderboard__total">Total: ${total}</span>
                   </li>`;
         })
         .join("");
-      list.innerHTML = descriptionListHTML;
+      list.innerHTML = listItems;
     }
+  };
+
+  const showTopKeywords = () => {
+    const text = textField.value;
+    const lowerCaseText = text.toLowerCase();
+    const words = lowerCaseText.match(wordRegEx);
+    const list = document.querySelector(".js-leaderboard-keywords");
+    showStatistic(words, list);
+  };
+
+  const showTopTwoWordsPhrases = () => {
+    const text = textField.value;
+    const lowerCaseText = text.toLowerCase();
+    const twoWordsRegEx = /[a-zA-Z\d]{1,} [a-zA-Z\d]{1,}/g;
+    const words = lowerCaseText.match(twoWordsRegEx);
+    const list = document.querySelector(".js-leaderboard-two-words");
+    showStatistic(words, list);
+  };
+
+  const showTopThreeWordsPhrases = () => {
+    const text = textField.value;
+    const lowerCaseText = text.toLowerCase();
+    const threeWordsRegEx = /[a-zA-Z\d]{1,} [a-zA-Z\d]{1,} [a-zA-Z\d]{1,}/g;
+    const words = lowerCaseText.match(threeWordsRegEx);
+    const list = document.querySelector(".js-leaderboard-three-words");
+    showStatistic(words, list, "leaderboard__item--three-words");
   };
 
   const loadText = () => {
@@ -124,6 +182,8 @@
       countWords();
       countLines();
       showTopKeywords();
+      showTopTwoWordsPhrases();
+      showTopThreeWordsPhrases();
     }
   };
 
@@ -132,6 +192,23 @@
   textField.addEventListener("input", countWords);
   textField.addEventListener("input", countLines);
   textField.addEventListener("input", saveText);
-  textField.addEventListener("input", showTopKeywords);
+
+  /**
+   * Prevent lagging when typing.
+   *
+   * If I use "input" event then the application
+   * will start lagging every time I am typing.
+   *
+   * Another way that I was trying was to add "blur" event
+   * to the `textField`. But, that could make me confused because
+   * the application does not react. Then, I realized that I forgot
+   * to remove my blinking cursor from the `textField`.
+   */
+  showTopWordsButton.addEventListener("click", () => {
+    showTopKeywords();
+    showTopTwoWordsPhrases();
+    showTopThreeWordsPhrases();
+  });
+
   document.addEventListener("DOMContentLoaded", loadText);
 })();
